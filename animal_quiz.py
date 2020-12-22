5ac369d84f5bb9c4a325c41a2cfa7154c3c28b3f
Larry6oy <peng.larry@outlook.com>
2020-12-22 17:00:04 -0500

    My first animal quiz(extra hard! Only 1 life!)

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,7 +1,19 @@
 score = 0
+def check_guess(guess, answer):
+    global score
+    if guess.lower() == answer.lower():
+        print('Correct answer')
+        score = score + 1
 print('gess the animal!')
 guess1 = input('Which bear lives at the North pole?')
-check_guess(guess1, 'poler bear')
+print(guess1)
+check_guess(guess1, 'polar bear')
 guess2 = input('Which is the bigest animal?')
+print(guess2)
 check_guess(guess2, 'blue whale')
-guess3 = input('')
+guess3 = input('Which bear lives in Alaska?')
+print(guess3)
+check_guess(guess3, 'kodiak bear')
+
+
+print('Your score is ' +str(score))
