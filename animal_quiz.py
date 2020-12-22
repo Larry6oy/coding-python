b16c0f5038b5e4d763cacedb42d528fbe99f8dd3
Larry6oy <peng.larry@outlook.com>
2020-12-22 16:09:05 -0500

    For my Animal quiz projects.

---
+++ animal_quiz.py
@@ -0,0 +1,7 @@
+score = 0
+print('gess the animal!')
+guess1 = input('Which bear lives at the North pole?')
+check_guess(guess1, 'poler bear')
+guess2 = input('Which is the bigest animal?')
+check_guess(guess2, 'blue whale')
+guess3 = input('')
