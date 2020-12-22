7e5298c420b4aa847ae7a25e955de68076bcc7fc
Larry6oy <peng.larry@outlook.com>
2020-12-21 19:56:21 -0500

    loopty loop update

---
+++ loop_workspace.py
@@ -0,0 +1,9 @@
+for counter in range(1, 11):
+    print('Python coders only!!!!')
+
+codes = 0
+answer = 'y'
+while answer == 'y':
+    codes = codes + 1
+    print(str(codes) + ' coding people!')
+    answer = input('Add another person? (y/n)')
