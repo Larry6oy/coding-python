215691755952c320beb5265e3c68da5b7a70970e
larry6oy <peng.larry@outlook.com>
2024-06-18 21:08:04 -0400

    ...

--- snap.py
+++ snap.py
@@ -2,11 +2,10 @@ import random
 import time
 from tkinter import Tk, Canvas, HIDDEN, NORMAL
 
-
 root = Tk()
 root.title('Snap')
-c = Canvas(root, width=400, height=400)
 
+c = Canvas(root, width=400, height=400)
 
 shapes = []
 
@@ -17,6 +16,23 @@ circle = c.create_oval(35, 20, 365, 350, outline='red', fill='red', state=HIDDEN
 shapes.append(circle)
 circle = c.create_oval(35, 20, 365, 350, outline='green', fill='green', state=HIDDEN)
 shapes.append(circle)
-circle = c.create_oval(35, 20, 365, 350, outline='blue', fill='blue', state=NORMAL)
+circle = c.create_oval(35, 20, 365, 350, outline='blue', fill='blue', state=HIDDEN)
 shapes.append(circle)
+
+
+rectangle = c.create_rectangle(35, 100, 365, 270, outline='black', fill='black', state=HIDDEN)
+shapes.append(rectangle)
+rectangle = c.create_rectangle(35, 100, 365, 270, outline='red', fill='red', state=HIDDEN)
+shapes.append(rectangle)
+rectangle = c.create_rectangle(35, 100, 365, 270, outline='green', fill='green', state=HIDDEN)
+shapes.append(rectangle)
+rectangle = c.create_rectangle(35, 100, 365, 270, outline='blue', fill='blue', state=HIDDEN)
+shapes.append(rectangle)
+
+
+square = c.create_rectangle(35, 20, 365, 350, outline='black', fill='black', state=HIDDEN)
+shapes.append(square)
+square = c.create_rectangle(35, 20, 365, 350, outline='black', fill='black', state=HIDDEN)
+shapes.append(square)
+
 c.pack()
